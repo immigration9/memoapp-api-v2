a96994451c77d0871a1541cc452108602aabd602
MinGu Lee <projgotham18@gmail.com>
2020-02-21 11:24:32 +0900

    memos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const labels = require('./routes/labels');
-// const memos = require('./routes/memos');
+const memos = require('./routes/memos');
 
 const db = require('./models/database');
 
@@ -10,10 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/labels', labels);
-// app.use('/memos', memos);
+app.use('/memos', memos);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('DramanCompany Memoapp API');
   console.log(`Now listening on port ${port}`);
-})
+});
