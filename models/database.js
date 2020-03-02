899c066b40d6a408ca134c5f4dcf4bebc148c225
MinGu Lee <projgotham18@gmail.com>
2020-03-02 14:26:36 +0900

    fixed db notfound error

--- models/database.js
+++ models/database.js
@@ -3,36 +3,36 @@ const path = require('path');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-module.exports = () => {
-  const dbDirectory = path.join(__dirname, '../db');
+const dbDirectory = path.join(__dirname, '../db');
 
-  /**
-   * Creates Database directory if it doesn't exist.
-   */
-  if (!fs.existsSync(dbDirectory)) {
-    fs.mkdirSync(dbDirectory);
-  }
+/**
+ * Creates Database directory if it doesn't exist.
+ */
+if (!fs.existsSync(dbDirectory)) {
+  fs.mkdirSync(dbDirectory);
+}
 
-  const isTest = process.env.ENVIRONMENT === 'test';
-  const fileDirectory = path.join(
-    __dirname,
-    `../db/${isTest ? 'test_db' : 'db'}.json`
-  );
+const isTest = process.env.ENVIRONMENT === 'test';
+const fileDirectory = path.join(
+  __dirname,
+  `../db/${isTest ? 'test_db' : 'db'}.json`
+);
 
-  /**
-   * Test Environment Only
-   * * remove preexisting test-db file
-   */
-  if (isTest && fs.existsSync(fileDirectory)) {
-    fs.unlinkSync(fileDirectory);
-  }
+/**
+ * Test Environment Only
+ * * remove preexisting test-db file
+ */
+if (isTest && fs.existsSync(fileDirectory)) {
+  fs.unlinkSync(fileDirectory);
+}
 
-  const adapter = new FileSync(fileDirectory);
-  const db = low(adapter);
+const adapter = new FileSync(fileDirectory);
+const db = low(adapter);
 
-  db.defaults({
-    labels: [],
-    memos: [],
-    labelsToMemos: [],
-  }).write();
-};
+db.defaults({
+  labels: [],
+  memos: [],
+  labelsToMemos: [],
+}).write();
+
+module.exports = db;
