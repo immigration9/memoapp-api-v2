9bf047cece34c82a741e420e3beeff50e8e04bb8
myoungho.pak <qkraudghgh@gmail.com>
2020-03-02 14:00:21 +0900

    CORS 미들웨어 추가

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const labels = require('./routes/labels');
 const memos = require('./routes/memos');
 
-const db = require('./models/database');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
 
 app.use('/labels', labels);
 app.use('/memos', memos);
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log('DramanCompany Memoapp API');
   console.log(`Now listening on port ${port}`);
